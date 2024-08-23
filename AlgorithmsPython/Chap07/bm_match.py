74709587e4af51ed750804bad2e66095baea4eee
SunHarvey <sunharvey19@gmail.com>
2024-08-23 09:12:34 +0800

    add Chap07

---
+++ AlgorithmsPython/Chap07/bm_match.py
@@ -0,0 +1,17 @@
+def bm_match(txt: str, pat: str) -> int:
+    skip = [None] * 256
+
+    for pt in range(256):
+        skip[pt] = len(pat)
+    for pt in range(len(pat)):
+        skip[ord(pat[pt])] = len(pat) - pt - 1
+
+    while pt < len(txt):
+        pp = len(pat) - 1
+        while txt[pt] == pat[pp]:
+            if pp == 0:
+                return pt
+            pt -= 1
+            pp -= 1
+        pt += skip[ord(txt[pt])] if skip[ord(txt[pt])] > len(pat) - pp else len(pat) - pp
+    return -1
